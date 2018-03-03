9bf6b66c14f4043ee1fd1981accd8ccbf23dc424
Kevin <Kevin@PC.lan>
2018-03-02 23:08:26 -0700

    1.0

---
+++ src/eva1_1_tipos_primitivos/Principal.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_1_tipos_primitivos;
+
+/**
+ *
+ * @author Kevin Ortiz
+ */
+public class Principal{
+
+    public static void main(String[] args) {
+        int iNumMax = Integer.MAX_VALUE;
+        System.out.println(iNumMax);
+        int iNumMin = Integer.MIN_VALUE;
+        System.out.println(iNumMin);
+        iNumMax++;
+        System.out.println(iNumMax);
+        iNumMin--;
+        System.out.println(iNumMin);
+        long iNumerote = Integer.MAX_VALUE;
+        System.out.println(iNumerote);
+        iNumerote++;
+        System.out.println("Numerote: " + iNumerote);
+
+    }
+
+}
